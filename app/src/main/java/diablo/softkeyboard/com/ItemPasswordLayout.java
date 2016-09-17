cd070e6ead13f78b666bd95f85a3792204b0c682
Diablo <shenyonghe525@gmail.com>
2016-09-17 12:22:02 +0800

    删除edttext

--- app/src/main/java/diablo/softkeyboard/com/ItemPasswordLayout.java
+++ app/src/main/java/diablo/softkeyboard/com/ItemPasswordLayout.java
@@ -3,7 +3,6 @@ package diablo.softkeyboard.com;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
@@ -12,7 +11,6 @@ import android.widget.RelativeLayout;
  */
 public class ItemPasswordLayout extends RelativeLayout
         implements MyNumKeyBoard.IOnKeyboardListener {
-    private EditText editText;
     private ImageView[] imageViews;//使用一个数组存储密码框
     private MyNumKeyBoard myNumKeyBoard;
     private StringBuffer stringBuffer = new StringBuffer();//存储密码字符
@@ -31,8 +29,6 @@ public class ItemPasswordLayout extends RelativeLayout
         super(context, attrs, defStyleAttr);
         imageViews = new ImageView[PASSWORD_COUNT];
         View view = View.inflate(context, R.layout.soft_keyboard_lay, this);
-
-        editText = (EditText) findViewById(R.id.item_edittext);
         myNumKeyBoard = (MyNumKeyBoard) findViewById(R.id.view_keyboard);
         myNumKeyBoard.shuffleKeyboard();
         myNumKeyBoard.setIOnKeyboardListener(this);
@@ -42,8 +38,6 @@ public class ItemPasswordLayout extends RelativeLayout
         imageViews[3] = (ImageView) findViewById(R.id.item_password_iv4);
         imageViews[4] = (ImageView) findViewById(R.id.item_password_iv5);
         imageViews[5] = (ImageView) findViewById(R.id.item_password_iv6);
-
-        editText.setCursorVisible(false);//将光标隐藏
     }
 
     private InputCompleteListener inputCompleteListener;
@@ -78,7 +72,4 @@ public class ItemPasswordLayout extends RelativeLayout
         void inputComplete(String password);
     }
 
-    public EditText getEditText() {
-        return editText;
-    }
 }
